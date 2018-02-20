1f942f4e599a93f5d8442c045ff45acd5c536685
interglobalmedia <interglobalmedia@gmail.com>
2018-02-20 07:48:25 -0500

    First commit
    Add all files

---
+++ moviejs.js
@@ -0,0 +1,27 @@
+/* http://www.omdbapi.com/?i=tt3896198&apikey=60f7bdd3  */
+function getData() {
+    const movie = document.getElementById('movie').value;
+    const span = container.getElementsByTagName('span').textContent;
+    const url = 'http://www.omdbapi.com/?apikey=60f7bdd3&t=' + movie;
+    //Using Javascript to get AJAX data
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            console.log(JSON.parse(this.responseText));
+            var data = JSON.parse(this.responseText);
+            document.getElementById("actors").innerHTML = data.Actors;
+            document.getElementById("year").innerHTML = data.Year;
+            document.getElementById("runtime").innerHTML = data.RunTime;
+            document.getElementById("rated").innerHTML = data.Rated;
+            document.getElementById("genre").innerHTML = data.Genre;
+            document.getElementById("poster").setAttribute("src", data.Poster);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+const button = document.getElementById('btn');
+button.addEventListener('click', () => {
+    getData();
+})
