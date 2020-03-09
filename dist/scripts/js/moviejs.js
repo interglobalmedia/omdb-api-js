43e624f2bcd8e2f581ef574ad7502065a63247e8
interglobalmedia <interglobalmedia@gmail.com>
2020-03-09 13:52:45 -0400

    Add first dist build

---
+++ dist/scripts/js/moviejs.js
@@ -0,0 +1,28 @@
+/* http://www.omdbapi.com/?i=tt3896198&apikey=60f7bdd3  */
+function getData() {
+    const movie = document.getElementById('movie').value;
+    const container = document.getElementById('container');
+    const span = container.getElementsByTagName('span').textContent;
+    const url = 'https://www.omdbapi.com/?apikey=60f7bdd3&t=' + movie;
+    const xhttps = new XMLHttpRequest();
+    xhttps.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            console.log(JSON.parse(this.responseText));
+            const data = JSON.parse(this.responseText);
+            document.getElementById("actors").innerHTML = data.Actors;
+            document.getElementById("year").innerHTML = data.Year;
+            document.getElementById("runtime").innerHTML = data.Runtime;
+            document.getElementById("rated").innerHTML = data.Rated;
+            document.getElementById("genre").innerHTML = data.Genre;
+            document.getElementById("poster").setAttribute("src", data.Poster);
+        }
+    };
+    xhttps.open("GET", url, true);
+    xhttps.send();
+}
+
+const button = document.getElementById('btn');
+button.addEventListener('click', () => {
+    getData();
+})
