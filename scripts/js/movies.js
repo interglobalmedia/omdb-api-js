f658774a57bb99e73f14f07a64a35f0a8844228c
Maria Campbell <interglobalmedia@gmail.com>
2021-12-30 15:36:32 -0500

    Updates

--- scripts/js/movies.js
+++ scripts/js/movies.js
@@ -2,20 +2,42 @@
 function getData() {
   const movie = document.getElementById('movie').value;
   const url = `https://www.omdbapi.com/?apikey=60f7bdd3&t=${movie}`;
+  const urlUndefined = `https://www.omdbapi.com/?apikey=60f7bdd3&t=undefined`;
   const xhttps = new XMLHttpRequest();
   xhttps.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      console.log(this.responseText);
-      console.log(JSON.parse(this.responseText));
-      const data = JSON.parse(this.responseText);
-      document.getElementById('director').innerHTML = data.Director;
-      document.getElementById('actors').innerHTML = data.Actors;
-      document.getElementById('year').innerHTML = data.Year;
-      document.getElementById('runtime').innerHTML = data.Runtime;
-      document.getElementById('rated').innerHTML = data.Rated;
-      document.getElementById('genre').innerHTML = data.Genre;
-      document.getElementById('poster').setAttribute('src', data.Poster);
-    }
+    try {
+      if (document.querySelector('.error-message').innerHTML === `undefined` || document.querySelector('.error-message').innerHTML === `Incorrect IMDb ID.
+
+`) {
+          document.querySelector('.error-message').innerHTML = ``;
+      }
+      if (this.readyState === 4 && this.status === 200) {
+          console.log(this.responseText);
+          console.log(JSON.parse(this.responseText));
+          const data = JSON.parse(this.responseText);
+          document.getElementById('movie').innerHTML = data.Title;
+          document.getElementById('director').innerHTML = data.Director;
+          document.getElementById('actors').innerHTML = data.Actors;
+          document.getElementById('year').innerHTML = data.Year;
+          document.getElementById('runtime').innerHTML = data.Runtime;
+          document.getElementById('rated').innerHTML = data.Rated;
+          document.getElementById('genre').innerHTML = data.Genre;
+          document.getElementById('poster').setAttribute('src', data.Poster);
+      } else if (this.status === 404 || urlUndefined) {
+          const data = JSON.parse(this.responseText);
+          const message = data["Error"];
+          const title = document.querySelector('[name=movie]');
+          title.value = ``;
+          const errorPara = document.querySelector('.error-message');
+          errorPara.style.display = 'block';
+          errorPara.style.color = 'red';
+          errorPara.innerHTML = `${message}`;
+        } else {
+          return;
+        }
+      } catch (error) {
+          console.log(error);
+      }
   };
   xhttps.open('GET', url, true);
   xhttps.send();
